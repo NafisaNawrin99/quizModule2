c4b6e8f5aba176ce4392f6865e28f373eed8cc5d
NafisaNawrin99 <nafisanawrin.21@gmail.com>
2024-12-30 20:39:10 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+$questions = [
+     ['question' => 'What is 2 + 2?', 'correct' => '4'],
+    
+    ['question' => 'What is the capital of France?', 'correct' => 'Paris'],
+    
+    ['question' => 'Who wrote "Hamlet"?','correct' => 'Shakespeare']
+
+];              
+
+$answers = [];
+foreach ($question as $index => $question){
+    echo ( $index+1) . "." . $questions['question']."\n";
+    $answer[]=trim (readline("Your answer: "));
+}
+
+function evaluateQuiz( array $questions, array $answers):int{
+    $score = 0;
+    foreach ($question as $index => $question){
+        if ($answer[index]==$question['correct']){
+            $score++;
+
+        }
+    }
+    return $score;
+}
+
+git init
+README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NafisaNawrin99/quizModule2.git
+git push -u origin main
+
+$score = evaluateQuiz($questions, $answers);
+echo "\n Your score is $score out of ". count($question).".\n";
+if ($score === count($question)){
+    echo "You are a genius!\n" ;
+}
+else if ( $score>1){
+    echo "Good Effort! \n";
+}
+else { echo "Try hard! \n"; }
